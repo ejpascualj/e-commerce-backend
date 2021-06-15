e0714390f10f6df98d0dcf00cccf963e74aa31cf
Emilio P <ejpascualj@gmail.com>
2021-06-14 23:23:39 -0500

    fixed issue with delete route in product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -121,7 +121,9 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
-  Product.destroy()
+  Product.destroy({
+    where: {id: req.params.id}
+  })
     .then((productData) => {
       if (!productData) {
         res.status(404).json({ message: 'Product not found with ID' });
